9297b5f8ba830b3844329ff84d899ed87946fad8
Amr Hassan <amr.hassan@gmail.com>
2008-10-17 03:32:58 +0000

    nomsg

--- trunk/installer.sh
+++ trunk/installer.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-wget http://pylast.googlecode.com/files/pyLast-0.2b12.tar.gz
+wget http://pylast.googlecode.com/files/pyLast-0.2b13-1.tar.gz
 tar -xf pyLast*.tar.gz
 cd pyLast*
 python setup.py install
 
+cd ..
 mkdir -vp /usr/share/lastagent
 cp -vf lastagent /usr/bin
 chmod a+x /usr/bin/lastagent
